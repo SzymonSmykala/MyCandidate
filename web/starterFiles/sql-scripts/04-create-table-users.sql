5c839aa5eca4afde7e66e05d303d2eea27a3033f
Szymon Smykała <ctk7000@gmail.com>
2018-01-08 19:33:30 +0100

    1. Added candidates' answers sql view 2. Edited users table

--- web/starterFiles/sql-scripts/04-create-table-users.sql
+++ web/starterFiles/sql-scripts/04-create-table-users.sql
@@ -3,7 +3,7 @@ create table users
 	user_id int not null auto_increment
 		primary key,
 	email varchar(45) not null,
-	password varchar(45) not null,
+	password varchar(45)  null,
 	first_name varchar(45) null,
 	last_name varchar(45) null,
 	role varchar(45) default 'USER' not null

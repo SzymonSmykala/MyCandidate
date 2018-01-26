24d38f89fd4151934279335b8ba81d72b394d986
Szymon Smykała <ctk7000@gmail.com>
2018-01-26 15:15:30 +0100

    add-candidate-details trigger, create view-result-for-user and one function

---
+++ web/starterFiles/sql-scripts/triggers/increment-candidate-stats.sql
@@ -0,0 +1,15 @@
+DROP TRIGGER IF EXISTS incrementCandidatesStats;
+CREATE TRIGGER incrementCandidatesStats
+AFTER INSERT ON my_candidate.users
+FOR EACH ROW
+  BEGIN
+    INSERT INTO `candidates-stats`(date, numberOfNewCandidates) VALUES (DATE(NOW()),0)
+    ON DUPLICATE KEY UPDATE date = VALUES(date);
+
+    CASE WHEN NEW.role = "CANDIDATE"
+      THEN
+        UPDATE `candidates-stats`
+        SET numberOfNewCandidates = numberOfNewCandidates + 1
+        WHERE `candidates-stats`.date = DATE(NOW());
+    END CASE;
+  END;
